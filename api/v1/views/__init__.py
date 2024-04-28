f469c157b2da35e127924814ec8a6dc425c94611
samwel Okoth <samwelokoth8@gmail.com>
2024-04-28 13:52:46 +0300

    updated v3 clone

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,6 +3,6 @@ from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-# Wildcard import of views, don't worry about PEP8 warning
-from api.v1.views.index import *
+# Import the newly created states view
+from api.v1.views.states import *
 
---
+++ api/v1/views/states.py
@@ -0,0 +1,51 @@
+# api/v1/views/states.py
+from api.v1.views import app_views
+from flask import jsonify, abort, request
+from models import storage, State
+
+@app_views.route('/states', methods=['GET'])
+def get_states():
+    states = storage.all(State).values()
+    return jsonify([state.to_dict() for state in states])
+
+@app_views.route('/states/<state_id>', methods=['GET'])
+def get_state(state_id):
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    return jsonify(state.to_dict())
+
+@app_views.route('/states/<state_id>', methods=['DELETE'])
+def delete_state(state_id):
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    storage.delete(state)
+    storage.save()
+    return jsonify({})
+
+@app_views.route('/states', methods=['POST'])
+def create_state():
+    if not request.json:
+        abort(400, 'Not a JSON')
+    if 'name' not in request.json:
+        abort(400, 'Missing name')
+    data = request.get_json()
+    state = State(**data)
+    state.save()
+    return jsonify(state.to_dict()), 201
+
+@app_views.route('/states/<state_id>', methods=['PUT'])
+def update_state(state_id):
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    if not request.json:
+        abort(400, 'Not a JSON')
+    data = request.get_json()
+    for key, value in data.items():
+        if key not in ['id', 'created_at', 'updated_at']:
+            setattr(state, key, value)
+    state.save()
+    return jsonify(state.to_dict())
+
